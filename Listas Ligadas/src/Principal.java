fa7a9520f008f842cf4d95945f8c23dc17ff4758
tiochencho <95369056+tiochencho@users.noreply.github.com>
2022-10-12 17:13:22 -0500

    Update Principal.java

--- Listas Ligadas/src/Principal.java
+++ Listas Ligadas/src/Principal.java
@@ -9,25 +9,25 @@ public class Principal {
        
         Listas<Integer> y = new Listas<>();
         
-        System.out.println("------------------Método estaVacia()------------------"); 
+        System.out.println("-Método estaVacia()-"); 
         
         y.estaVacia();
         
-        System.out.println("------------------Método getTamanio()------------------"); 
+        System.out.println("-Método getTamanio()-"); 
      
         y.getTamanio();
         
-        System.out.println("------------------Método agregarAlFinal()------------------");
+        System.out.println("-Método agregarAlFinal()-");
      
         y.agregarFinal(8);
         y.transversal();
         
-        System.out.println("------------------Método agregarAlInicio()------------------");
+        System.out.println("-Método agregarAlInicio()-");
     
         y.agregarInicio(23);
         y.transversal();
         
-        System.out.println("------------------Método agregarDespuesDe()------------------");
+        System.out.println("-Método agregarDespuesDe()-");
        
         y.agregarSiguiente(8, 44);
         y.transversal();
@@ -39,41 +39,41 @@ public class Principal {
         y.agregarFinal(10);
         y.agregarFinal(14);
         y.agregarSiguiente(10, 21);
-        System.out.println("------------------Agregamos más elementos a la lista------------------");
+        System.out.println("-Agregamos más elementos a la lista-");
         y.transversal();
         
-        System.out.println("------------------Método eliminar()------------------");
+        System.out.println("-Método eliminar()-");
       
         y.eliminar(3);
         y.transversal();
         
-        System.out.println("------------------Método eliminarElFinal()-------------------");
+        System.out.println("-Método eliminarElFinal()-");
      
         y.eliminarFinal();
         y.transversal();
         
-        System.out.println("------------------Método eliminarElPrimero()-------------------");
+        System.out.println("-Método eliminarElPrimero()-");
      
         y.eliminarInicio();
         y.transversal();
         
-        System.out.println("------------------Método buscar()--------------------");
+        System.out.println("-Método buscar()-");
         
         y.buscador(6);
         
-        System.out.println("------------------Método actualizar()------------------");
+        System.out.println("-Método actualizar()-");
         
         y.actualizar(8, 1);
         
-        System.out.println("------------------Método getTamanio()------------------");
+        System.out.println("-Método getTamanio()-");
       
         y.getTamanio();
         
-        System.out.println("------------------Método transversal()------------------");
+        System.out.println("-Método transversal()-");
         
         y.transversal();
         
-        System.out.println("------------------Método estaVacia()------------------"); 
+        System.out.println("-Método estaVacia()-"); 
         
         y.estaVacia();
         
